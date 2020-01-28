18eef4f8be634f1b8f7793758b9d7a115477b799
pagenotfound91 <zheng.jin@husky.neu.edu>
2020-01-28 04:54:36 -0800

    Create user.service.client.js

---
+++ src/main/webapp/services/user.service.client.js
@@ -0,0 +1,48 @@
+function AdminUserServiceClient() {
+    this.createUser = createUser;
+    this.findAllUsers = findAllUsers;
+    this.findUserById = findUserById;
+    this.deleteUser = deleteUser;
+    this.updateUser = updateUser;
+    this.url = 'https://wbdv-generic-server.herokuapp.com/api/sjzheng/users';
+    var self = this;
+
+    function createUser(user) {
+        return fetch(self.url, {
+            method: 'POST',
+            body: JSON.stringify(user),
+            headers: {
+                "content-type": "application/json"
+            }
+        }).then(response => response.json())
+    }
+
+    function findAllUsers() {
+        return fetch(self.url)
+            .then(response => response.json())
+    }
+
+
+    function findUserById(id) {
+        return fetch(`${self.url}/${id}`)
+            .then((response) => {
+                return response.json()
+            })
+    }
+
+    function updateUser(id, user) {
+        return fetch(`${self.url}/${id}`, {
+            method: 'PUT',
+            body: JSON.stringify(user),
+            headers: {
+                "content-type": "application/json"
+            }
+        }).then(response => response.json())
+    }
+
+    function deleteUser(id) {
+        return fetch(`${self.url}/${id}`, {
+            method: "DELETE"
+        })
+    }
+}
